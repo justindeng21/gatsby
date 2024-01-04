0df99c2691aaec1104ec1956aaaee34073683c27
justindeng21 <justin.n.deng@gmail.com>
2024-01-04 08:13:08 -0700

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `testucp`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-vanilla-extract"]
+};
